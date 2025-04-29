1d209946850e7eda826049912b4b961bc74707a5
santamei <santameiisantikaa@gmail.com>
2025-04-29 12:21:37 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -42,4 +42,37 @@ class Auth extends CI_Controller{
             }
         }
     }
+    public function process_login(){
+        $username=$this->input->post('username');
+        $password=$this->input->post('password');
+        $user=$this->User_model->check_user($username, $password);
+        if($user){
+            $this->session->set_userdata([
+                'user_id'=>$user->id,
+                'username'=>$user->username,
+                'role'=>$user->role,
+                'logged_id' =>TRUE
+            ]);
+
+            $this->redirect_by_role($user->role);
+    }else{
+        $this->session->set_flashdata('error', 'Username dan Password salah');
+        redirect('Auth/login');
+    }
+}
+private function redirect_by_role($role){
+    switch($role){
+        case 'admin':
+            redirect('dashboard');
+            break;
+            case 'user':
+                redirect('user/dashboard');
+                break;
+            default:
+            redirect('Auth/login');
+    }
+}
+public function logout(){
+    $this->session->sess_destroy();
+    redirect('Auth/login');
 }
